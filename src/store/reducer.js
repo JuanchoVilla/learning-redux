0e4536a779e977fadcae43424b59f6689a68f545
Juan Carmona <xjuanchovillax@gmail.com>
2019-06-07 21:42:46 -0600

    Add full redux capabilities to the app
    I removed the counterChangedHandler method and implemented all it's functionality in redux. I also updated the package.json file to fix some version conflitcts in the app.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,7 +3,16 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-  return reducer
+  if (action.type === 'INCREMENT') {
+    return { counter: state.counter + 1 }
+  } else if (action.type === 'DECREMENT') {
+    return { counter: state.counter - 1 }
+  } else if (action.type === 'ADD') {
+    return { counter: state.counter + action.value }
+  } else if (action.type === 'SUB') {
+    return { counter: state.counter - action.value }
+  }
+  return state;
 }
 
-export default reducer
+export default reducer;
