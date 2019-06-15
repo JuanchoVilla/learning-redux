88df54b7189585a098fdf51474f053e04b53f031
Juan Carmona <xjuanchovillax@gmail.com>
2019-06-14 21:54:07 -0600

    Add Redux devtools
    I added a middlware logger and added redux web devtools to the project

--- src/index.js
+++ src/index.js
@@ -4,15 +4,31 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 
-const reducer = combineReducers({ ctr: counterReducer,
-  res: resultReducer })
+const logger = store => (
+  next => (
+    action => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    }
+  )
+)
 
-const store = createStore(reducer);
+const reducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
